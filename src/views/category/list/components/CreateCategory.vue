32abeda32ff377356d396ff59015838387ea6834
Minh Khoa <adamkhoa03@gmail.com>
2025-03-11 23:39:08 +0700

    edit category

--- src/views/category/list/components/CreateCategory.vue
+++ src/views/category/list/components/CreateCategory.vue
@@ -1,34 +1,21 @@
 <script setup lang="ts">
 import constants from '@/constants.ts';
-import { ref } from 'vue';
-import { useForm } from 'vee-validate';
-import { object, string, number } from 'yup';
-import { toTypedSchema } from '@vee-validate/yup';
-import { useI18n } from 'vue-i18n';
+import { useCategoryForm } from '@/views/category/list/components/configs/useCategoryForm.ts';
 
-const { t } = useI18n();
-
-//Create schema and default value
-const schema = object({
-  name: string().required().default('').label(t('category.categoryName')),
-  status: number().required().default(1).label(t('category.categoryStatus')),
-  description: string().required().default('').label(t('category.description'))
-});
-const { errors, defineField, handleSubmit, resetForm } = useForm({
-  validationSchema: toTypedSchema(schema)
-});
-
-//Defined and binding field
-const [name, categoryNameAttr] = defineField('name');
-const [status, categoryStatusAttr] = defineField('status');
-const [description, categoryDescriptionAttr] = defineField('description');
-
-const dialog = ref(false);
-
-const listCategoryStatus = ref([
-  { value: 1, title: 'Hoạt động' },
-  { value: 0, title: 'Không hoạt động' }
-]);
+const {
+  t,
+  name,
+  categoryNameAttr,
+  status,
+  categoryStatusAttr,
+  description,
+  categoryDescriptionAttr,
+  errors,
+  handleSubmit,
+  resetForm,
+  dialog,
+  listCategoryStatus
+} = useCategoryForm();
 
 const addCategory = () => {};
 const onSubmit = handleSubmit((value) => {
