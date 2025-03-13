69bab4adea7415a7db499b07b289715def1a3708
Minh Khoa <adamkhoa03@gmail.com>
2025-03-14 01:05:27 +0700

    rename vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/base-v3/",
+  base: "/mobile-shop/",
   plugins: [
     vue({
       template: {
